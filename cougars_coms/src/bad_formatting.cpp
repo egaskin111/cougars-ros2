461c0d8096aa9f361c29d1bc8f89adea34c04551
Eli Gaskin <egaskin2@byu.edu>
2025-09-25 12:48:37 -0600

    fix?

--- cougars_coms/src/bad_formatting.cpp
+++ cougars_coms/src/bad_formatting.cpp
@@ -7,24 +7,9 @@ int x=0;    ;;;
     } else {   }	
 
 
-for(int i=0;i<1;i++){  					 
+for(int i=0;i<1;i++){    					 
     ; // pointless semicolon
-  x += (0) ; 
-    }	  
-
-return 0;;}
-int    maincoolfunc2(){ 	  
-int x=0;    ;;;     
-   if((x==0)) 
-    {
-		// weird indentation and tabs
-        // do nothing
-    } else {   }	
-
-
-for(int i=0;i<1;i++){  					 
-    ; // pointless semicolon
-  x += (0) ; 
+  x +=  (0) ; 
     }	  
 
 return 0;;}
